db50b4315650f2a9960c3d4f6c3455740fdde85e
lemontree-mxh <maxuhongzzia@163.com>
2018-11-09 17:36:29 +0800

    learn class and object transfer

--- xx/src/EmployeeTest.java
+++ xx/src/EmployeeTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class EmployeeTest {
+    public static void main(String args[]){
+        Employee empOne = new Employee("Apple");
+        Employee empTwo = new Employee("Pear");
+
+        empOne.empAge(26);
+        empOne.empDesignation("Senior Software Engineer");
+        empOne.empSalary(10000);
+        empOne.printEmployee();
+
+        empTwo.empAge(21);
+        empTwo.empDesignation("Software Engineer");
+        empTwo.empSalary(5000);
+        empTwo.printEmployee();
+    }
 }
--- xx/src/Puppy.java
+++ xx/src/Puppy.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Puppy {
+    int puppyAge;
+    public Puppy(String name){
+        //构造器，必须与类同名，可有多个
+        System.out.println("Passed name is :" + name);
+    }
+
+    public void setAge(int age){
+        //方法
+        puppyAge = age;
+    }
+
+    public int getAge(){
+        System.out.println("Puppy's age is :" + puppyAge);
+        return puppyAge;
+    }
+
+
+    public static void main(String[] args){
+        //创建对象
+        Puppy myPuppy = new Puppy("Oscar");
+        //设置puppyAge的值
+        myPuppy.setAge(2);
+        myPuppy.getAge();
+        System.out.println("Variable Value :" + myPuppy.puppyAge);
+    }
 }
